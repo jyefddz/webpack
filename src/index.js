fea74af75431b0d15391195f1faddbe915c52a74
jyefddz <1065697146@qq.com>
2022-06-27 21:01:01 +0800

    jquery引入

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@
 // console.log('实现轮播图的业务逻辑')
 // // tab栏切换的逻辑
 // console.log('实现tabs标签页的逻辑')
+import './banner.js'
+import './tabs.js'
+
+import $ from 'jquery'
+$('#swiper').css('background', 'pink')
