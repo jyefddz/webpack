5e1bc4cee4d9b368154a15e10e99e2f398e1797b
jyefddz <1065697146@qq.com>
2022-06-27 21:32:03 +0800

    webpack5处理图片

--- src/main.js
+++ src/main.js
@@ -10,3 +10,15 @@ $('#swiper').css('background', 'pink')
 
 // import './styles/index.css';
 import './styles/index.less';
+
+import imgUrl from './assets/1.gif';
+const img = document.createElement('img');
+img.src = imgUrl
+document.body.appendChild(img)
+
+
+
+import imgUrl1 from './assets/logo_small.png';
+const img1 = document.createElement('img');
+img1.src = imgUrl1
+document.body.appendChild(img1)
